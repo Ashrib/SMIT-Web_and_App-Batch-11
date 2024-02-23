efe4803866b17be4f1d6be282e70a866f8e811ec
Asharib Ali <asharib920@gmail.com>
2024-02-23 23:26:58 +0500

    JS variables

--- 6. Javascript/1. Intro/app.js
+++ 6. Javascript/1. Intro/app.js
@@ -1,4 +1,59 @@
-alert("Hello world!");
+// alert("Hello world!");
+// window.alert("Hello world!");
+
+
+// variables for string
+var firstName = "hello world!";
+alert(firstName);
+
+firstName = "ahmed"; //change the value of firstName
+alert(firstName);
+
+
+var contact; //undefined
+contact = "contact";
+
+
+
+
+// variables for number
+var x = 5;
+var y = 5 + 5;
+var a = 90 + x;
+var b = x;
+
+var num = 5;
+num = y + 5;
+
+
+num = num + 5;
+
+// example of how to calculate percentage
+// formula to calculate percentage:  (obt marks)/total marks * 100 
+var myPerc = (y/100) * 100;
+alert(myPerc);
+
+
+// document.write(num);
+var t = 'hello'+'world';
+document.write("hello"+" world "); //concatenation with string
+
+
+var num1 = '90';
+// document.write(num1+5);
+
+
+
+
+document.write('5'*5);
+
+
+
+
+
+
+
+
 
 
 
