bc7c8689c2e486e13211c180d2f95d770627e651
Asharib Ali <asharib920@gmail.com>
2024-02-26 23:11:00 +0500

    maths expressions and concatenation string

--- 6. Javascript/1. Variables/app.js
+++ 6. Javascript/1. Variables/app.js
@@ -4,10 +4,10 @@
 
 // variables for string
 var firstName = "hello world!";
-alert(firstName);
+// alert(firstName);
 
 firstName = "ahmed"; //change the value of firstName
-alert(firstName);
+// alert(firstName);
 
 
 var contact; //undefined
@@ -31,7 +31,7 @@ num = num + 5;
 // example of how to calculate percentage
 // formula to calculate percentage:  (obt marks)/total marks * 100 
 var myPerc = (y/100) * 100;
-alert(myPerc);
+// alert(myPerc);
 
 var t = 'hello'+'world'; //concatenation in variable
 document.write(t); //concatenation with string
@@ -49,12 +49,54 @@ document.write(num1+5); //concatenation with string and number
 document.write('5'*5); // do maths calculation
 
 
+console.log('Number');
+console.log(7);
+console.log(true);
+
+
+var firstName = 'John';
+var first_name = 'John';
+var _id = 'John';
+var num_1 = 'John';
+var rose = 'John';
+
+
+
+var console1 = 'John';
+var isSignIn = false;
+
+console.log(rose);
 
 
 
 
 
+var score = 5;
+console.log(score);
 
+
+
+
+
+// score = score+2;
+// console.log(score);
+score++;
+
+++score;
+console.log(score);
+
+score = score - 1;
+score--;
+--score;
+console.log(score);
+
+
+var num2 = score++;
+console.log(num2);
+
+
+var score2 = score +1;
+console.log(score2);
 
 
 
---
+++ 6. Javascript/2. Math expressions/app.js
@@ -0,0 +1,35 @@
+
+
+
+
+var totalCost = (1+(3/(5+3))) * (4^5);
+console.log(totalCost)
+
+
+
+var message = "Thanks";
+var mess2 = " for your";
+var mess3 =  "input!";
+
+
+
+console.log(message + mess2 + mess3);
+console.log(message+"for coming")
+
+
+// console.log(5+5+"for coming"+5+5)
+
+
+
+console.log(5+5+'hello world'+5+5)
+
+
+
+
+
+console.log('hello','world')
+
+console.log(5+5+'hello world', 5+5)
+
+
+
